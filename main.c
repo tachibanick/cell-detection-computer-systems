9614ade2a91959c93234e91e22692cc409508acb
Nicholas Lentfer Tachibana Kristiansen <nicholas@kristiansenz.com>
2024-09-11 13:26:05 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,49 +1,51 @@
-//To compile (linux/mac): gcc cbmp.c main.c -o main.out -std=c99
-//To run (linux/mac): ./main.out example.bmp example_inv.bmp
+// To compile (linux/mac): gcc cbmp.c main.c -o main.out -std=c99
+// To run (linux/mac): ./main.out example.bmp example_inv.bmp
 
-//To compile (win): gcc cbmp.c main.c -o main.exe -std=c99
-//To run (win): main.exe example.bmp example_inv.bmp
+// To compile (win): gcc cbmp.c main.c -o main.exe -std=c99
+// To run (win): main.exe example.bmp example_inv.bmp
 
 #include <stdlib.h>
 #include <stdio.h>
 #include "cbmp.h"
 
-//Declaring the array to store the image (unsigned char = unsigned 8 bit)
+// Declaring the array to store the image (unsigned char = unsigned 8 bit)
 unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS];
 unsigned char output_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS];
 unsigned char processed_image[BMP_WIDTH][BMP_HEIGTH];
 
-//Function to invert pixels of an image (negative)
-void invert(unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS], unsigned char output_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS]){
+// Function to invert pixels of an image (negative)
+void invert(unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS], unsigned char output_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS])
+{
   for (int x = 0; x < BMP_WIDTH; x++)
   {
     for (int y = 0; y < BMP_HEIGTH; y++)
     {
       for (int c = 0; c < BMP_CHANNELS; c++)
       {
-      output_image[x][y][c] = 255 - input_image[x][y][c];
+        output_image[x][y][c] = 255 - input_image[x][y][c];
       }
     }
   }
 }
 
-
-void greyscale(unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS]){
-    for (int x = 0; x < BMP_WIDTH; x++){
-        for (int y = 0; y < BMP_HEIGTH; y++)
-        {   
-            unsigned char* pixel = input_image[x][y];
-            char avg_color = (pixel[0] + pixel[1] + pixel[2])/3;
-            for (int c = 0; c < BMP_CHANNELS; c++)
-            {
-              processed_image[x][y] = avg_color;
-            }
-        }
-        
+void greyscale(unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS])
+{
+  for (int x = 0; x < BMP_WIDTH; x++)
+  {
+    for (int y = 0; y < BMP_HEIGTH; y++)
+    {
+      unsigned char *pixel = input_image[x][y];
+      char avg_color = (pixel[0] + pixel[1] + pixel[2]) / 3;
+      for (int c = 0; c < BMP_CHANNELS; c++)
+      {
+        processed_image[x][y] = avg_color;
+      }
     }
+  }
 }
 
-void output_from_greyscale(unsigned char processed_image[BMP_WIDTH][BMP_HEIGTH]){
+void output_from_greyscale(unsigned char processed_image[BMP_WIDTH][BMP_HEIGTH])
+{
   for (int x = 0; x < BMP_WIDTH; x++)
   {
     for (int y = 0; y < BMP_HEIGTH; y++)
@@ -53,34 +55,34 @@ void output_from_greyscale(unsigned char processed_image[BMP_WIDTH][BMP_HEIGTH])
         output_image[x][y][c] = processed_image[x][y];
       }
     }
-  } 
+  }
 }
 
-//Main function
-int main(int argc, char** argv)
+// Main function
+int main(int argc, char **argv)
 {
-  //argc counts how may arguments are passed
-  //argv[0] is a string with the name of the program
-  //argv[1] is the first command line argument (input image)
-  //argv[2] is the second command line argument (output image)
+  // argc counts how may arguments are passed
+  // argv[0] is a string with the name of the program
+  // argv[1] is the first command line argument (input image)
+  // argv[2] is the second command line argument (output image)
 
-  //Checking that 2 arguments are passed
+  // Checking that 2 arguments are passed
   if (argc != 3)
   {
-      fprintf(stderr, "Usage: %s <output file path> <output file path>\n", argv[0]);
-      exit(1);
+    fprintf(stderr, "Usage: %s <output file path> <output file path>\n", argv[0]);
+    exit(1);
   }
 
   printf("Example program - 02132 - A1\n");
 
-  //Load image from file
+  // Load image from file
   read_bitmap(argv[1], input_image);
 
-  //Run inversion
-  invert(input_image,output_image);
+  // Run inversion
+  invert(input_image, output_image);
   greyscale(input_image);
   output_from_greyscale(processed_image);
-  //Save image to file
+  // Save image to file
   write_bitmap(output_image, argv[2]);
 
   printf("Done!\n");
