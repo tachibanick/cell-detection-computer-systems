aca9616cb2bb24ee50b1c07da2c5da1c7a7f2504
Peter Graugaard <git@graugaard.me>
2024-09-25 14:44:44 +0200

    35/35 84

--- main.c
+++ main.c
@@ -25,7 +25,7 @@ double cpu_time_used;
 // prints step by step
 #define print_cell_detection 0
 // ratio between how many cells inside detection to in the filtration layer
-#define RATIO_INSIDE_OUT 4
+#define RATIO_INSIDE_OUT 3
 // how many cells must be at least detected inside the detection layer
 #define MIN_INSIDE_CELLS 87
 
